920c751a222ccd6a01d6ea30cdb011a4e98fa106
Sasakiai <szpakowiczk@gmail.com>
2026-02-02 11:20:57 +0100

    Created LeafNode and unit tests for it

---
+++ src/leafnode.py
@@ -0,0 +1,23 @@
+from htmlnode import HTMLNode
+
+
+class LeafNode(HTMLNode):
+    def __init__(
+        self,
+        tag: str | None = None,
+        value: str | None = None,
+        props: dict[str, str] | None = None,
+    ):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("All leaf nodes must have a value")
+
+        if not self.tag:
+            return self.value
+
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"Tag: {self.tag}, Value: {self.value}, Props: {self.props}"
