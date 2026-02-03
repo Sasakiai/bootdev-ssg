e939e11e14f5fc8e21b5073512622fd9dd141ace
Sasakiai <szpakowiczk@gmail.com>
2026-02-03 21:12:47 +0100

    Copy static files recursively

--- src/main.py
+++ src/main.py
@@ -1,9 +1,34 @@
+import os
+import shutil
+
 from textnode import TextNode, TextType
 
 
+def copy_static(source_path, destination_path):
+    os.mkdir(destination_path)
+    dir_content = os.listdir(source_path)
+
+    for item in dir_content:
+        src_item_path = os.path.join(source_path, item)
+        dst_item_path = os.path.join(destination_path, item)
+
+        if os.path.isfile(src_item_path):
+            shutil.copy(src_item_path, dst_item_path)
+        else:
+            copy_static(src_item_path, dst_item_path)
+
+
+def copy_content(src, dst):
+    if os.path.exists(dst):
+        shutil.rmtree(dst)
+
+    copy_static(src, dst)
+
+
 def main():
-    n = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(n)
+    src = "./static"
+    dst = "./public"
+    copy_content(src, dst)
 
 
 if __name__ == "__main__":
